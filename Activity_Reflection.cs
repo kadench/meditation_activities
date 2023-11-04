6332ffeae966be77883b42751b2c9d1a25ebf2f0
kadench <kadench@byui.edu>
2023-11-04 15:08:47 -0600

    Message

--- Activity_Reflection.cs
+++ Activity_Reflection.cs
@@ -38,19 +38,22 @@ class ReflectionActivity : Activity {
         return khRandomString;
     }
 
-    private string KhGatherActivityResources() {
+    // Gets the prompt for the current session
+    private string KhGetPrompt() {
         int khRandomPromptIndex = KhGetRandomIndexFromList(_khPrompts);
         string khRandomPrompt = KhChooseRandomPrompt(khRandomPromptIndex);
         return khRandomPrompt;
     }
 
+    // Goes through the activity
     public void StartActivity() {
     KhStartingMessage(_khActivityName);
         Thread.Sleep(1000);
-    string khPrompt = KhGatherActivityResources();
-    var startTime = DateTime.UtcNow;
+    string khPrompt = KhGetPrompt();
+    var khStartTime = DateTime.UtcNow;
     
-    while (DateTime.UtcNow - startTime < TimeSpan.FromSeconds(_khDuration)) {
+    // Do the activity for the set duration but let them finish whatever question they're on.
+    while (DateTime.UtcNow - khStartTime < TimeSpan.FromSeconds(_khDuration)) {
         Console.Clear();
         Console.WriteLine("Please think about the following prompt. You will be asked questions about it later:");
         Thread.Sleep(1000);
@@ -60,10 +63,13 @@ class ReflectionActivity : Activity {
         Console.ReadLine();
 
         for (int i = 0; i < _khQuestions.Count; i++) {
-            if (DateTime.UtcNow - startTime >= TimeSpan.FromSeconds(_khDuration)) {
+            
+            // Checks if time is up before going onto the next question.
+            if (DateTime.UtcNow - khStartTime >= TimeSpan.FromSeconds(_khDuration)) {
                 break;
             }
 
+            // Waits and asks the next question
             Thread.Sleep(1000);
             Console.Write($"{_khQuestions[i]}: ");
             Console.ReadLine();
@@ -71,11 +77,12 @@ class ReflectionActivity : Activity {
             KhCreateAnimation(_khAnimation, 2);
         }
     }
+    // Only say you've done it if the user answers one question. Otherwise the choice might've been a mistake.
     if (_khQuestionsThrough > 0) {
         Thread.Sleep(1000);
         Console.WriteLine($"Time's up! You answered {_khQuestionsThrough} questions out of 9! Well done!");
         Thread.Sleep(1000);
-        Console.Write($"Take some time to reflect over your answers or write them down. They will not be saved! (Press enter when ready to end.): ");
+        Console.Write($"Take some time to reflect over your answers or write them down. They will not be saved! (Press enter when you're ready to end.): ");
         Console.ReadLine();
     }
     
